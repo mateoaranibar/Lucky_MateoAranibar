34e5292a30711fb921e846dea643eb99459cc254
Mateo <a2020110318@estudiantes .upsa.edu.bo.com>
2021-10-13 20:04:41 -0400

    Migration implementado

---
+++ SegundoParcial/Migrations/20211014000152_AddModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SegundoParcial.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lucky",
+                columns: table => new
+                {
+                    SuerteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Detalle = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Enlace = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lucky", x => x.SuerteId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lucky");
+        }
+    }
+}
